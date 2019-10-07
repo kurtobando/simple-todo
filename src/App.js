aff1a3fd352b602bca747f8c65538c9a7bb65392
Kurt Obando <kurtobando.developer@gmail.com>
2019-10-07 21:52:18 +0800

    change App from functional to class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 
 
-function App() {
-	return ( 
-		<div>
-			Hello Todo App	
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				Hello Todo App	
+			</div>
+		)
+	}
 }
 
 
