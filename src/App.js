06dbec098a32889bf6c7141767e8b6d9c62f73f7
Kurt Obando <kurtobando.developer@gmail.com>
2019-10-07 21:50:16 +0800

    remove ; and add spacing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
+
+
 
 function App() {
 	return ( 
-		<div className = "App" >
+		<div>
 			Hello Todo App	
 		</div>
 	);
 }
+
+
 
 export default App;
